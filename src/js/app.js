4ca4ef1827c1a90036c038a0f9ac636a58a5e367
imonweb <contact@imonweb.co.uk>
2021-11-11 11:02:24 +0000

    spinner load

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,12 @@
+/*  Spinner */
+window.onload = () => {
+  setTimeout( () => {
+    document.querySelector("body").classList.add('display');
+  }, 4000)
+}
+
+/*************/
+
 document.querySelector('.hamburger-menu').addEventListener('click', () => {
   document.querySelector('.container').classList.toggle('change');
 })
